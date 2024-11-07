082e98a8d808c831f5c1efea2a5db62505b1a611
CHEGEBB <chegephil24@gmail.com>
2024-11-07 14:31:19 +0300

    Refactor CSS files and add background gradient to main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.scss'
+import BgStars from "./images/bg-stars.svg"
+import PatternHills from "./images/pattern-hills.svg"
+import IconFacebook from "./images/icon-facebook.svg"
+import IconInstagram from "./images/icon-instagram.svg"
+import IconPinterest from "./images/icon-pinterest.svg"
 
 function App() {
 
   return (
-    <div className="main-container bg-slate-600">
-      <h1 className='text-right'>We're launching soon</h1>
+    <div className="main-container">
+      <div className="top-container">
+        <image 
+        src={BgStars}
+        alt="Background Stars"
+        className="bg-stars "
+        />
+      </div>
+      <div className="bottom-container"></div>
     </div>
   )
 }
